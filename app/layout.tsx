5bbc6ac1fd7639ba596929b13b4318c4999a34ef
Gestao App <dev@gestao-app.com>
2025-10-19 15:38:05 -0400

    fix: criar arquivo dark-mode.css separado com prioridade máxima

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./dark-mode.css";
 import Navigation from "./components/Navigation";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
